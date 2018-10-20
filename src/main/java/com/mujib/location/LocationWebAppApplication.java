e6b84d8e191711fa9343c94c0cf6e0ae10476251
Mujib Ansari <ansarimujiburrehman@gmail.com>
2018-10-20 13:29:54 +0530

    test

---
+++ src/main/java/com/mujib/location/LocationWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.mujib.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationWebAppApplication.class, args);
+	}
+}
