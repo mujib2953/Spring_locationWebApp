33258bcd6e21e885c09e417fb9865df57c8d41a4
Mujib Ansari <ansarimujiburrehman@gmail.com>
2018-10-22 22:13:18 +0530

    Created Entity Class

--- src/main/java/com/mujib/location/entities/Location.java
+++ src/main/java/com/mujib/location/entities/Location.java
@@ -1,5 +1,57 @@
 package com.mujib.location.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "location_table")
 public class Location {
+
+	@Id
+	private int id;
+	
+	@Column(name="code")
+	private String Code;
+	private String name;
+	private String type;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return Code;
+	}
+
+	public void setCode(String code) {
+		Code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Location [id=" + id + ", Code=" + Code + ", name=" + name + ", type=" + type + "]";
+	}
 
 }
