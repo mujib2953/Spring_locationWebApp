31bfebc7a9804d6b7baf38da0381a3c64b8fbf04
Mujib Ansari <ansarimujiburrehman@gmail.com>
2018-10-22 22:37:15 +0530

    Created first JUnit Test

--- src/test/java/com/mujib/location/LocationWebAppApplicationTests.java
+++ src/test/java/com/mujib/location/LocationWebAppApplicationTests.java
@@ -2,15 +2,29 @@ package com.mujib.location;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.mujib.location.entities.Location;
+import com.mujib.location.repos.LocationRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LocationWebAppApplicationTests {
-
+	
+	@Autowired
+	private LocationRepository locationRepos;
+	
 	@Test
-	public void contextLoads() {
+	public void creatLocation() {
+		Location location = new Location();
+		
+		location.setName("New York City");
+		location.setCode("NYC");
+		location.setType("URBAN");
+		
+		locationRepos.save( location );
 	}
 
 }
