a202c1bf5466a0c22ae48bd8458b2a8272b49fd2
Mujib Ansari <ansarimujiburrehman@gmail.com>
2018-10-27 23:56:35 +0530

    Repos -->method added to reuturn type and count of type

--- src/main/java/com/mujib/location/repos/LocationRepository.java
+++ src/main/java/com/mujib/location/repos/LocationRepository.java
@@ -1,9 +1,15 @@
 package com.mujib.location.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mujib.location.entities.Location;
 
 public interface LocationRepository extends JpaRepository<Location, Integer> {
+
+	@Query(value = "SELECT type, count(*) FROM location_table GROUP BY type", nativeQuery = true)
+	public List<Object[]> findTypeAndTypeCount();
 
 }
